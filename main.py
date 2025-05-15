2a0f9ce96b16d984923d779b6647e87fa5d2245e
atbdm <atbdmora@gmail.com>
2025-05-15 03:52:17 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import math
+
+
+def add(x, y):
+    """Function to add two numbers"""
+    return x + y
+
+
+
+print("Select operation:")
+print("1. Add")
+
+
+
+while True:
+    choice = input("Enter choice (1- ADD MORE ): ")
+
+    if choice in ('1'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print("Result:", add(num1, num2))
+
+        break
+
+
+    else:
+        print("Invalid input. Please enter a valid number (1/2/3/4).")
